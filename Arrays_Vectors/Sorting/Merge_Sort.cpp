82945e71705cfee35a01f099fe4e324a5b16c447
Mukul Chandrakant Mahadik <37911144+MukuFlash03@users.noreply.github.com>
2020-04-23 22:32:44 +0530

    Create Merge_Sort.cpp

---
+++ Arrays_Vectors/Sorting/Merge_Sort.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(vector<int> &arr, int start, int mid, int end)
+{
+    int temp[end-start+1];
+    int i = start, j = mid+1, k=0;
+
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp[k] = arr[i];
+            k++;
+            i++;
+        }   
+        else
+        {
+            temp[k] = arr[j];
+            k++;
+            j++;
+        }
+    }
+
+    while (i <= mid)
+    {
+        temp[k] = arr[i];
+        k++;
+        i++;
+    }
+
+    while(j <= end)
+    {
+        temp[k] = arr[j];
+        k++;
+        j++;
+    }
+
+    for (i = start; i <= end; i++)
+        arr[i] = temp[i-start];
+
+}
+
+void mergeSort(vector <int> &arr, int start, int end)
+{
+    if (start < end)
+    {
+        int mid = (start + end)/2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid+1, end);
+        merge(arr, start, mid, end);
+    }
+}
+
+int main()
+{
+    vector <int> arr;
+    int a, size;
+    cin >> size;
+
+    for (int i = 0; i < size; i++)
+    {   
+        cin >> a;
+        arr.push_back(a);
+    }
+
+    mergeSort(arr, 0, size-1);
+    cout << "Sorted Data:\n";
+
+    for (int i : arr) // i = 0; i < arr.size(); i++ and cout arr
+        cout << i << " ";
+    
+    return 0;
+}
